cc1b7b6119315b0a408a3d051cc4386559f4a01d
Shameem <shameem.pullaratt@gmail.com>
2023-10-13 20:10:36 +0200

    refactored main.cpp

--- main.cpp
+++ main.cpp
@@ -2,112 +2,18 @@
 #include <vector>
 #include <string>
 
-void printMenu()
-{
-    std::cout << "1: Print help" << std::endl;
-    std::cout << "2: Print exchange stats" << std::endl;
-    std::cout << "3: Place an ask" << std::endl;
-    std::cout << "4: Place a bid" << std::endl;
-    std::cout << "5: Print wallet" << std::endl;
-    std::cout << "6: Continue" << std::endl;
-    std::cout << "----" << std::endl;
-}
-
-int getUserOption()
-{
-    int userOption;
-    std::cout << "Type in 1-6" << std::endl;
-    std::cin >> userOption;
-
-    return userOption;
-}
-
-void printErrorMsg()
-{
-    std::cout << "No way!" << std::endl;
-    std::cout << "Type in 1-6" << std::endl;
-}
-
-void printHelp()
-{
-    std::cout << "Help - choose options from the menu" << std::endl;
-    std::cout << "and follow the on screen instructions." << std::endl;
-}
-
-void printMarketStats()
-{
-    std::cout << "Exchange stats - waite..." << std::endl;
-    std::cout << "I have to finish other lesson to get this :)" << std::endl;
-}
-
-void enterAsk()
-{
-    std::cout << "Place an ask - are you sure!!!" << std::endl;
-    std::cout << "Can I finish other lessons before this :)" << std::endl;
-}
-
-void enterBid()
-{
-    std::cout << "Place a bid - Are you serious?" << std::endl;
-    std::cout << "Investments are subject to market risks :) :) " << std::endl;
-}
-
-void printWallet()
-{
-    std::cout << "Wallet - I can't believe it! its zeros now  " << std::endl;
-    std::cout << "Don't show up this to any one else :D" << std::endl;
-}
-
-void gotoNextTimeframe()
-{
-    std::cout << "Continue - Why not! " << std::endl;
-    std::cout << "You carry on..." << std::endl;
-}
-
-void processUserOption(int userOption)
-{
-    if ((userOption < 1) || (userOption > 6))
-    {
-        printErrorMsg();
-    }
-    if (userOption == 1)
-    {
-        printHelp();
-    }
-    if (userOption == 2)
-    {
-        printMarketStats();
-    }
-    if (userOption == 3)
-    {
-        enterAsk();
-    }
-    if (userOption == 4)
-    {
-        enterBid();
-    }
-    if (userOption == 5)
-    {
-        printWallet();
-    }
-    if (userOption == 6)
-    {
-        gotoNextTimeframe();
-    }
-}
-
+/* enum for type of the order in oderBook*/
 enum class OrderBookType
 {
     bid,
     ask
 };
 
-class OrderBookEntry // this is the name of the class
+/* OrderBookEntry  class to hold one entry fron oderBook*/
+class OrderBookEntry 
 {
-public: // parts of the class that can be seen from outside
-    /** Create a new OrderBookEntry with the price
-    * set to the sent value
-    */
+public: 
+    /** Create a new OrderBookEntry */
     OrderBookEntry(std::string _timestamp, std::string _product, OrderBookType _oderType, double _price, double _amount);
 
     std::string timestamp;
@@ -115,17 +21,19 @@ public: // parts of the class that can be seen from outside
     OrderBookType oderType;
     double price;
     double amount;
-}; // note the semi-colon
+}; 
 
+/* Implimentation of constructre */
 OrderBookEntry::OrderBookEntry(std::string _timestamp, std::string _product, OrderBookType _oderType, double _price, double _amount)
-    :   timestamp(_timestamp),
-        product(_product),
-        oderType(_oderType),
-        price(_price),
-        amount(_amount)
+    : timestamp(_timestamp),
+      product(_product),
+      oderType(_oderType),
+      price(_price),
+      amount(_amount)
 {
 }
 
+/* Calculating some basic stats by iterating OrderBookEntry*/
 double computeAveragePrice(std::vector<OrderBookEntry> &entries)
 {
     double avg = 0;
@@ -139,39 +47,29 @@ double computeAveragePrice(std::vector<OrderBookEntry> &entries)
 }
 double computeLowPrice(std::vector<OrderBookEntry> &entries)
 {
-    double lowPrice = 0;    
+    double lowPrice = 0;
     for (OrderBookEntry &e : entries)
     {
-        if(lowPrice > e.price)
+        if (lowPrice > e.price)
         {
             lowPrice = e.price;
         }
-    }    
+    }
     return lowPrice;
 }
 double computeHighPrice(std::vector<OrderBookEntry> &entries)
 {
-    double highPrice = 0;    
+    double highPrice = 0;
     for (OrderBookEntry &e : entries)
     {
-        if(highPrice < e.price)
+        if (highPrice < e.price)
         {
             highPrice = e.price;
         }
-    }    
+    }
     return highPrice;
 }
-double computePriceSpread(std::vector<OrderBookEntry> &entries)
-{
-    double avg = 0;
-    double total = 0;
-    for (OrderBookEntry &e : entries)
-    {
-        total += e.price;
-    }
-    avg = total / entries.size();
-    return avg;
-}
+
 
 int main()
 {
@@ -226,5 +124,5 @@ int main()
     //     std::cout << e.price << std::endl;
     // }
     double avg = computeAveragePrice(entries);
-    std::cout << "Average = "<< avg << std::endl;
+    std::cout << "Average = " << avg << std::endl;
 }
